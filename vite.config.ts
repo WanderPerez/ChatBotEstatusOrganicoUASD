dfc224a2fb7bcf6bfe7533249e8072a9dee8de18
WanderPerez <elmontro007@gmail.com>
2026-05-27 00:29:51 -0400

    Set base path to './' for relative routing
    Add base configuration for relative paths.

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // <--- ¡AÑADE ESTA LÍNEA AQUÍ! Asegura que las rutas sean relativas
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
